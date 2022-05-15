f2fbfda8ca794612bd333bb48bc8f9badf5dc163
HeeJi <posnov1@naver.com>
2022-05-15 22:52:10 +0900

    module

---
+++ module_time.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import time
+
+print("지금부터 5초 동안 정지합니다!")
+time.sleep(5)
+print("프로그램을 종료합니다")
+
---
+++ module_urllib.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+# 모듈을 읽어 들입니다.
+from urllib import request
+
+# urlopen() 함수로 구글의 메인 페이지를 읽습니다.
+target = request.urlopen("https://google.com")
+output = target.read()
+
+# 출력합니다.
+print(output)
+
